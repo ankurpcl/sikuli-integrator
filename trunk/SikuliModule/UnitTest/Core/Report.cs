b7b9df75b6e173f65b963d46e83aa5ad5ac6b936
mrproper86@gmail.com <mrproper86@gmail.com@2624e16e-f305-e902-ffaf-50e0a75688be>
2013-07-27 06:46:49 +0000

    Add unit test project and tests for existing methods

---
+++ trunk/SikuliModule/UnitTest/Core/Report.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.Core
+{
+    public class Report
+    {
+        public static void Error(string text)
+        {
+            text = "FAULT:\t" + text;
+            Write(text + " :(");
+            Assert.Fail();
+        }
+
+        public static void Pass(string text)
+        {
+            text = "PASS:\t" + text;
+            Write(text + " :)");
+        }
+
+        public static void Info(string text)
+        {
+            text = "I:\t" + text;
+            Write(text + " :|");
+        }
+
+        private static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ trunk/SikuliModule/UnitTest/TestCases/CustomSimilarityAndTimeout.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SikuliModule;
+using UnitTest.Core;
+
+namespace UnitTest.TestCases
+{
+    [TestClass]
+    public class CustomSimilarityAndTimeout : BaseTestCase
+    {
+        const int Timeout5S = 5 * 1000;
+        const float Similarity90 = 0.90f;
+
+        [TestMethod,
+        Description("Test Exists mechanism - similarity = 90% and timeout = 5s")]
+        public void TestExistsWithSimilarity90AndTimeout5()
+        {
+            if (!SikuliAction.Exists(pattern, Similarity90, Timeout5S).IsEmpty)
+            {
+                Report.Pass("Yep! It's there...");
+            }
+            else
+            {
+                Report.Error("Nope! It's gone...");
+            }
+        }
+
+        [TestMethod,
+        Description("Test Click mechanism - similarity = 90% and timeout = 5s")]
+        public void TestClickWithSimilarity90AndTimeout5()
+        {
+            try
+            {
+                SikuliAction.Click(pattern, Similarity90, Timeout5S);
+                Report.Pass("Yep! It's clicked...");
+            }
+            catch
+            {
+                Report.Error("Nope! It's NOT clicked...");
+            }
+        }
+
+        [TestMethod,
+        Description("Test Double Click mechanism - similarity = 90% and timeout = 5s")]
+        public void TestDoubleClickWithSimilarity90AndTimeout5()
+        {
+            try
+            {
+                SikuliAction.DoubleClick(extraPattern, Similarity90, Timeout5S);
+                Report.Pass("Yep! It's double clicked...");
+            }
+            catch
+            {
+                Report.Error("Nope! It's NOT double clicked...");
+            }
+        }
+
+        [TestMethod,
+        Description("Test Right Click mechanism - similarity = 90% and timeout = 5s")]
+        public void TestRightClickWithSimilarity90AndTimeout5()
+        {
+            try
+            {
+                SikuliAction.RightClick(pattern, Similarity90, Timeout5S);
+                Report.Pass("Yep! It's right clicked...");
+            }
+            catch
+            {
+                Report.Error("Nope! It's NOT right clicked...");
+            }
+        }
+
+        [TestMethod,
+        Description("Test Drag and Drop mechanism - similarity = 90% and timeout = 5s")]
+        public void TestDragAndDropWithSimilarity90AndTimeout5()
+        {
+            try
+            {
+                SikuliAction.DragAndDrop(extraPattern, pattern, Similarity90, Timeout5S);
+                Report.Pass("Yep! It's drag and dropped...");
+            }
+            catch
+            {
+                Report.Error("Nope! It's NOT drag and dropped...");
+            }
+        }
+    }
+}
