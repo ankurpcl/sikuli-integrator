ae920ecdbb2e1e19edd5b649261fc97f5fc97861
mrproper86 <mrproper86@gmail.com>
2013-07-27 08:48:44 +0000

    Update FindAll test case

--- SikuliModule/UnitTest/Core/BaseTestCase.cs
+++ SikuliModule/UnitTest/Core/BaseTestCase.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest.Core
@@ -13,13 +14,29 @@ namespace UnitTest.Core
         readonly protected string findAllPattern = Environment.CurrentDirectory + @"\pattern2.png";
         readonly protected string demo = Environment.CurrentDirectory + @"\demo.png";
 
+        public TestContext TestContext { set; get; }
+
         [TestInitialize]
         public void BeforeEach()
         {
             //Stop Debug Trace
             Debug.Close();
 
-            StartMSPaint(demo);
+            //Get current test case
+            var testCaseMetaData = this.GetType().GetMethod(TestContext.TestName);
+            if (testCaseMetaData == null)
+            {
+                throw new ArgumentNullException("testCaseMetaData");
+            }
+
+            //Try to get AppNotStartAttribute attribute
+            var appNotStartAttribute = testCaseMetaData.GetCustomAttribute<AppNotStartAttribute>(true);
+
+            //Start App if AppNotStartAttribute does not exists
+            if (appNotStartAttribute == null)
+            {
+                StartMSPaint(demo);
+            }
         }
 
         [TestCleanup]
